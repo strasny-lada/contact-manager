2919bbed73d551dce75ea8e9f0939ebac9e24974
Tomáš Vítek <vitekt@email.cz>
2022-08-21 13:37:49 +0200

    contact list - notice in modal

---
+++ tests/Serializer/ContactSerializerTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types = 1);
+
+namespace App\Serializer;
+
+use App\Entity\Contact;
+use App\Value\EmailAddress;
+use App\Value\PhoneNumber;
+use PHPUnit\Framework\TestCase;
+
+class ContactSerializerTest extends TestCase
+{
+
+    public function testSerializeToJson(): void
+    {
+        $contact = new Contact(
+            'Harry',
+            'Šroubek',
+            EmailAddress::fromString('harry@sroubek.com'),
+            PhoneNumber::fromString('987 654 321'),
+            'Pellentesque in sapien nunc. Pellentesque venenatis nibh ut porta dignissim.',
+        );
+
+        self::assertSame(
+            '{"name":"\u0160roubek Harry","notice":"Pellentesque in sapien nunc. Pellentesque venenatis nibh ut porta dignissim."}',
+            ContactSerializer::serializeToJson($contact),
+        );
+    }
+
+    public function testSerializeToJsonWithRequiredFieldsOnly(): void
+    {
+        $contact = new Contact(
+            'Petrulie',
+            'Svižná',
+            EmailAddress::fromString('petrulie@svizna.com'),
+            null,
+            null,
+        );
+
+        self::assertSame(
+            '{"name":"Svi\u017en\u00e1 Petrulie","notice":null}',
+            ContactSerializer::serializeToJson($contact),
+        );
+    }
+
+}
